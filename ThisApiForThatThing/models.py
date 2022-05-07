50d0d9106ab1741398a7a8c838b20cd8df343581
architrixs <archit@i2vsys.com>
2022-05-07 23:30:01 +0530

    style fix for chrome

--- ThisApiForThatThing/models.py
+++ ThisApiForThatThing/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 # Create your models here.
 class ApiModel(models.Model):
-    name = models.CharField(max_length=500)
+    name = models.CharField(max_length=100)
     link = models.CharField(max_length=500)
-    description = models.CharField(max_length=500)
-    type = models.CharField(max_length=100)
+    description = models.CharField(max_length=150)
+    type = models.CharField(max_length=50)
     tryMe = models.CharField(max_length=500)
-    auth = models.CharField(max_length=50)
+    auth = models.CharField(max_length=10)
     working = models.BooleanField(default=True)
     _id = models.IntegerField(primary_key=True)
