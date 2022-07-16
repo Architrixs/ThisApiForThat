85bdadb9eed6ae5ada54f96af9aecf6330c4ddd0
Architrixs <architsingh12376@gmail.com>
2022-07-16 14:34:42 +0530

    enable gzip

--- ApiForApi/settings.py
+++ ApiForApi/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"]
-print(BASE_DIR)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
