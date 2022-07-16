e6266a771f7ba513bf5fefa28768fc0dd1701996
Architrixs <architsingh12376@gmail.com>
2022-07-16 13:13:54 +0530

    fix static for deployment

--- ApiForApi/settings.py
+++ ApiForApi/settings.py
@@ -125,8 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATICFILES_DIRS = [
-    #BASE_DIR / "static"]
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    ]
 print(BASE_DIR)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
