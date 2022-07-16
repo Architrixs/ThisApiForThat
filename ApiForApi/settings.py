aee97cc9f1285de73573d6c56dc03b7af7df8371
Architrixs <architsingh12376@gmail.com>
2022-07-16 14:24:11 +0530

    add WhiteNoiseMiddleware

--- ApiForApi/settings.py
+++ ApiForApi/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ApiForApi.urls'
