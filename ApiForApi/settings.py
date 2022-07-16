d5602e5c1a9e4d8966774636660a287ca1a6e9a0
Architrixs <architsingh12376@gmail.com>
2022-07-16 17:22:52 +0530

    setting postgres config for deployment

--- ApiForApi/settings.py
+++ ApiForApi/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import environ
+import  dj_database_url
+import sys
+
 # Initialise environment
 env = environ.Env()
 environ.Env.read_env()
@@ -83,13 +86,15 @@ WSGI_APPLICATION = 'ApiForApi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+RUNNING_DEVSERVER = (len(sys.argv) > 1 and sys.argv[1] == 'runserver')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+if not RUNNING_DEVSERVER:
+    DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
