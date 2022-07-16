0850fa024ce5e84311f934860ac6133350ed1eef
Architrixs <architsingh12376@gmail.com>
2022-07-16 12:53:14 +0530

    files for heroku

--- ApiForApi/settings.py
+++ ApiForApi/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j@3q$^hwh^w--0e)_bo@c+_k0lq5xceb#v2!-8-$yik#d55_yu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.5', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
