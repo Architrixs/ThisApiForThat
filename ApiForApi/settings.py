d2edddbe2d7024a74e2c2c362b04dc2c06effb16
Architrixs <architsingh12376@gmail.com>
2022-08-07 20:52:05 +0530

    updating settings

--- ApiForApi/settings.py
+++ ApiForApi/settings.py
@@ -30,13 +30,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j@3q$^hwh^w--0e)_bo@c+_k0lq5xceb#v2!-8-$yik#d55_yu'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Application definition
 
