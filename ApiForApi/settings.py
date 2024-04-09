c77532c19b9fd2da55624b9ebe486ce552da1a3b
Architrixs <architsingh12376@gmail.com>
2024-04-09 21:20:41 +0530

    Allowed Host changed

--- ApiForApi/settings.py
+++ ApiForApi/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['*']
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
