31c38a6429d4e4cfa7ca84e6a579051c0a4211eb
Architrixs <architsingh12376@gmail.com>
2024-04-11 21:01:43 +0530

    change allowed host to specific host

--- ApiForApi/settings.py
+++ ApiForApi/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['thisapiforthat.onrender.com', '127.0.0.1']
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
